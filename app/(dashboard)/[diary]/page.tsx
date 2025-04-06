6485972d52749e6bf06fc49824e9a43d76ed2ba4
FonjiDaniel <fonjidaniel28@gmail.com>
2025-04-06 14:40:36 +0100

    build error fix

--- app/(dashboard)/[diary]/page.tsx
+++ app/(dashboard)/[diary]/page.tsx
@@ -1,28 +1,31 @@
-
 import React from "react";
 import { getDiaryContent } from "@/lib/actions/library";
 import { getServerAuthData } from "@/lib/actions/user";
 import DiaryClient from "./diaryClient";
 import type { Metadata } from 'next';
 
-
 interface PageProps {
-  params: {
+  params: Promise<{
     diary: string;
-  };
+  }>;
 }
 
+
 export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
- const  diaryId = params.diary
+  const resolvedParams = await params;
+  const diaryId = resolvedParams.diary;
 
   return {
     title: diaryId,
   };
 }
 
-export default async function Page({ params }: PageProps ) {
+
+export default async function Page({ params }: { params: Promise<{ diary: string }> }) {
+  const resolvedParams = await params;
+  const diaryId = resolvedParams.diary;
+
   const { token } = await getServerAuthData();
-  const diaryId  = params.diary
 
   if (!token) {
     return <p>Not authenticated.</p>;
@@ -35,7 +38,6 @@ export default async function Page({ params }: PageProps ) {
     console.error("Failed to fetch diary content:", error);
     return <p>Failed to load diary content. Please try again</p>;
   }
-
 
   return (
     <div className="flex min-h-screen flex-col items-center gap-4 py-4 max-sm:px-0">
